5122d0b6eb3b74ac54e979f5ffc976f72795933e
kevinbyers77 <kevinbyers77@users.noreply.github.com>
2025-08-24 17:37:55 +1000

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -9,4 +9,11 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
       <App />
     </HashRouter>
   </React.StrictMode>
+  // only register SW on production builds
+if ("serviceWorker" in navigator && import.meta.env.PROD) {
+  window.addEventListener("load", () => {
+    const swUrl = `${import.meta.env.BASE_URL}sw.js`; // resolves to /PocketCalisthenics/sw.js on Pages
+    navigator.serviceWorker.register(swUrl).catch(console.error);
+  });
+}
 );
